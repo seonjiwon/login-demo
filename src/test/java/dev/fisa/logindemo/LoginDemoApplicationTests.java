b2abcb3adbd78626e0bb6d471bfa332164a573b6
백민정 <ggamji09@naver.com>
2026-03-12 10:22:17 +0900

    first commit

---
+++ src/test/java/dev/fisa/logindemo/LoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fisa.logindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
